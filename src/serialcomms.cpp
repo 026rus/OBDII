eae21c3f92e3aa65ac06a33278aa68ef39f14261
026rus <026rus@gmail.com>
2015-03-11 23:59:47 -0500

    Here so more :)

--- src/serialcomms.cpp
+++ src/serialcomms.cpp
@@ -6,6 +6,8 @@
 #include "serialcomms.h"
 #include <iostream>
 #include <string>
+#include <QString>
+
 
 using namespace std;
 
@@ -24,7 +26,8 @@ namespace serial
     bool SerialComms::serialConnect(void)
 	{ 
         string input_device = "";
-        foreach (const QSerialPortInfo &info, QSerialPortInfo::availablePorts()) {
+        foreach (const QSerialPortInfo &info, QSerialPortInfo::availablePorts())
+        {
                cout << "Name        : " << info.portName().toStdString() << endl;
                cout << "Description : " << info.description().toStdString() << endl;
                cout << "Manufacturer: " << info.manufacturer().toStdString() << endl;
@@ -41,14 +44,130 @@ namespace serial
         return false;
 	}
 
-    bool SerialComms::sendCommand(const QByteArray &data) {
+    bool SerialComms::sendCommand(const QByteArray &data)
+    {
         if (!this->port->isOpen()) { return false; }
         if (-1 < port->write(data)) { return true; }
         return false;
 	}
 
-    QString SerialComms::readCommand() {
+    QString SerialComms::readCommand()
+    {
         if (!this->port->isOpen()) { return ""; }
 	    return QString("That One!");
 	}
-};
+
+/***********************************************/
+    int SerialComms::getRPM()
+    {
+        //QString comm = "01 0C"; // the code for rpm
+
+        QString retval = "1af8";
+        int x = 0;
+        bool ok;
+
+        x = (retval.toInt(&ok, 16))/4;
+
+        if(ok) return x;
+        else return -1;
+    }
+
+    int SerialComms::getTempEngin()
+    {
+
+        //QString comm = "01 05 1"; // the code Enginr Tempereture
+
+        QString retval = "1af8";
+        int x = 0;
+        bool ok;
+
+        x = (retval.toInt(&ok, 16)) - 40;
+
+        if(ok) return x;
+        else return -1;
+    }
+
+    QString SerialComms::getErr()
+    {
+
+        //QString comm = "01 01"; // the code for Error code
+        /*
+         *  41 01 respons to the request
+         *  81 number of corrent troble codes
+         * 	81 - 80 or (129)- 120
+         * 	most segnificant bit indicate that the  Ceck Engine Light on or of.
+         *
+         *
+         */
+
+        QString retval = "41 01 81 07 65 04";
+        QString str;
+
+        QString r_str;
+        QString num_of_Errs;
+        QString err_codes;
+        QString str1;
+        QString str2;
+        QString str3;
+
+        retval = "41 01 83 07 65 04";
+
+        /*
+         * > 03 is will give you all the actual troble codes
+         *  like 01 33 00 00 00 00
+         *  most of it is junk we need only  0133
+         * 	and 0 = P0 and rest is as is
+         *
+         * 	 so the code will be P0133
+         *
+         *
+         */
+
+        /*
+         * here I just plaing around trying to see how it will work and what
+         * to do
+         */
+
+
+
+        retval.replace(" ","");
+
+        str = retval.replace(" ","");
+
+        r_str = retval.left(4);
+
+        str = retval;
+
+        return str;
+    }
+
+/***********************************************/
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
