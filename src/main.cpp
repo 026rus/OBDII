162b23c6f6e5b56bd8e56d47ed32f4c122476910
Zac Slade <krakrjak@gmail.com>
2015-04-08 14:49:42 -0500

    Fix problem in merge

--- src/main.cpp
+++ src/main.cpp
@@ -9,29 +9,20 @@
 
 
 using namespace serial;
-void parse(char code[]){
-/*****************************************************
- //RapidJSON working search of char array
-    const char json[] = " { \"P0001\" : \"TEST 1\", \"P0002\" : \"TEST 2\", \"P0003\" : \"TEST 3\" } ";
-    rapidjson::Document document;  // Default template parameter uses UTF8 and MemoryPoolAllocator.
 
-    #if 0
-        // "normal" parsing, decode strings to new buffers. Can use other input stream via ParseStream().
+int main(int argc, char** argv) {
     // this is to choose if you want a command line or
     // GUI so far if you pass antthing then it will bring command line
     // and if you do not pass anything the you get GUI
     // !!!!!! please stop deliting it to :)
-  if (argc < 2)
-    {
+    if (argc < 2) {
         // GUI interfase by defolte if no argument passed
-            return;
+	return;
         QApplication a(argc, argv);
         MainWindow w;
         w.show();
         return a.exec();
-    }
-    else
-    {
+    } else {
         CarCoreApp *app = new CarCoreApp(argc, &argv);
         qDebug() << "Application has launched.";
         app->run();
@@ -41,11 +32,5 @@ void parse(char code[]){
         #endif
         return app->exec();
     }
-    //CarCoreApp *app = new CarCoreApp(argc, &argv);
     qDebug() << "Application has launched.";
-    //app->run();
-    ParseJson *parser = new ParseJson("U0001");
-    //parse("P0002");
-    //return app->exec();
-    return 1;
 }
